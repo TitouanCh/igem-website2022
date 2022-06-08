a8f848d046ab0a25c26cf41be133be889ee8e986
TitouanCh <titouan.azert@wadsa.com>
2022-06-08 16:47:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+<title>CO2 Cure</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="/style.css">
+<link rel="icon" type="image/png" href="/favicon.png"/>
+<meta name="Description" content="Description">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/x-icon" href="/img/small-logo-black.png">
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+</style>
+</head>
+
+<body>
+
+<div class="top-bar"><img id="small-logo" src="/img/small-logo-white.png"></br>CO2 Cure</div>
+<div class="top-whitespace"></div>
+
+<main id="main">
+
+<img id="big-logo" src="/img/logo.png">
+<hr>
+
+<div id="short-presentation">
+<p><img id="team-picture" src="/img/team-pic.jpeg">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+</div>
+
+<hr>
+
+<br/>
+
+<ul class="navbar-nav" role="tablist">
+<li class="navbar-item active"><a class="navbar-link">Wiki</a></li>
+<li class="navbar-item"><a class="navbar-link">Articles</a></li>
+<li class="navbar-item"><a class="navbar-link" href="/our-team/">Our team</a></li>
+</ul>
+
+<div class="article-list">
+<div class="article-item">
+<h2>Article Title</h2>
+<img id="team-picture" src="/img/strepto.png">
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+</br>
+<a href="yo">Continue Reading</a>
+</div>
+
+<div class="article-item">
+<h2>Article Title</h2>
+<img id="team-picture" src="/img/strepto.png">
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+</br>
+<a href="yo">Continue Reading</a>
+</div>
+
+<div class="article-item">
+<h2>Article Title</h2>
+<img id="team-picture" src="/img/strepto.png">
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+</br>
+<a href="yo">Continue Reading</a>
+</div>
+
+<div class="article-item">
+<h2>Article Title</h2>
+<img id="team-picture" src="/img/strepto.png">
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+</br>
+<a href="yo">Continue Reading</a>
+</div>
+
+<div class="article-item">
+<h2>Article Title</h2>
+<img id="team-picture" src="/img/strepto.png">
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
+</br>
+<a href="yo">Continue Reading</a>
+</div>
+</div>
+
+</main>
+
+</body>
+
+</html>
---
+++ our-team/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+<title>CO2 Cure</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="/style.css">
+<link rel="icon" type="image/png" href="/favicon.png"/>
+<meta name="Description" content="Description">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" type="image/x-icon" href="/img/small-logo-black.png">
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+</style>
+</head>
+
+<body>
+
+<div class="top-bar"><img id="small-logo" src="/img/small-logo-white.png"></br>CO2 Cure</div>
+<div class="top-whitespace"></div>
+
+<main id="main">
+
+<img id="big-logo" src="/img/logo.png">
+<hr>
+
+<div id="short-presentation">
+<p><img id="team-picture" src="/img/team-pic.jpeg">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+</div>
+
+<hr>
+
+<br/>
+
+<ul class="navbar-nav" role="tablist">
+<li class="navbar-item"><a class="navbar-link" href="/">Wiki</a></li>
+<li class="navbar-item"><a class="navbar-link">Articles</a></li>
+<li class="navbar-item active"><a class="navbar-link">Our team</a></li>
+</ul>
+
+<div class="article-list">
+
+<div class="article-item">
+<h2>Bob</h2>
+<img id="team-picture" src="/img/personne.png" style="margin-right:4%;">
+<ul>
+<li>23 years old</li>
+<li>undergraduate</li>
+<li>loves dolphins</li>
+<li>studying biology</li>
+</ul>
+</p>
+</br>
+</div>
+
+<div class="article-item">
+<h2>Sophie</h2>
+<img id="team-picture" src="/img/personne.png" style="margin-right:4%;">
+<ul>
+<li>21 years old</li>
+<li>undergraduate</li>
+<li>loves tigers</li>
+<li>studying biology and mathematics</li>
+</ul>
+</p>
+</br>
+</div>
+
+<div class="article-item">
+<h2>Martin</h2>
+<img id="team-picture" src="/img/personne.png" style="margin-right:4%;">
+<ul>
+<li>???</li>
+<li>???</li>
+<li>???</li>
+<li>???</li>
+</ul>
+</p>
+</br>
+</div>
+
+</main>
+
+</body>
+
+</html>
